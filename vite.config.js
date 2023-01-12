81f5eefe49a72b57636472bc8fb6175447cd892e
sbellina <shawn.bellina@gmail.com>
2023-01-12 03:29:00 -0300

    s

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
  plugins: [vue()],
- publicPath: '/public/'
+ publicPath: '/public/',
+ alias: {
+    '@': fileURLToPath(new URL('./src', import.meta.url))
+  }
  })
 
 // export default defineConfig({
